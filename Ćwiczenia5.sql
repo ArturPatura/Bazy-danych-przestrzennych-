0e8053102b98c4089bf3c868863f483322b12602
ArturPatura <56359448+ArturPatura@users.noreply.github.com>
2020-05-07 22:11:40 +0200

    Create Ćwiczenia5.sql

---
+++ Ćwiczenia5.sql
@@ -0,0 +1,58 @@
+CREATE DATABASE gis5;
+
+CREATE EXTENSION postgis;
+
+CREATE TABLE obiekty
+(
+	nazwa varchar(40),
+	geometria geometry
+);
+
+--obiekt1
+INSERT INTO obiekty(nazwa, geometria) 
+VALUES ('obiekt1', ST_GeomFromText('COMPOUNDCURVE(LINESTRING(0 1, 1 1), CIRCULARSTRING(1 1, 2 0, 3 1), 
+CIRCULARSTRING(3 1, 4 2, 5 1), LINESTRING(5 1, 6 1))', -1));
+
+--obiekt2
+INSERT INTO obiekty(nazwa, geometria) 
+VALUES('obiekt2', ST_GeomFromText('CURVEPOLYGON(COMPOUNDCURVE(LINESTRING(10 6, 14 6), CIRCULARSTRING(14 6, 16 4, 14 2),
+ CIRCULARSTRING(14 2, 12 0, 10 2), LINESTRING(10 2, 10 6)),CIRCULARSTRING(11 2, 12 1, 13 2, 12 3, 11 2))', -1));
+
+--obiekt3
+INSERT INTO obiekty VALUES('obiekt3', ST_GeomFromText('POLYGON((10 17, 7 15, 12 13, 10 17))',-1));
+
+--obiekt4
+INSERT INTO obiekty(nazwa, geometria) 
+VALUES ('obiekt4', ST_GeomFromText('LINESTRING(20 20, 25 25, 27 24, 25 22, 26 21, 22 19, 20.5 19.5)', -1));
+
+--obiekt5
+INSERT INTO obiekty(nazwa, geometria) 
+VALUES ('obiekt5', ST_GeomFromText('GEOMETRYCOLLECTION(MULTIPOINT(30 30 59 , 38 32 234))', -1));
+
+--obiekt6
+INSERT INTO obiekty(nazwa, geometria) 
+VALUES ('obiekt6', ST_GeomFromText('GEOMETRYCOLLECTION(POINT(4 2), LINESTRING(1 1, 3 2))', -1));
+
+
+--ZADANIA
+
+--1. Wyznacz pole powierzchni bufora o wielkości 5 jednostek, który został utworzony wokół najkrótszej linii łączącej obiekt 3 i 4.
+--2. Zamień obiekt4 na poligon. Jaki warunek musi być spełniony, aby można było wykonać to zadanie? Zapewnij te warunki.
+--3. W tabeli obiekty, jako obiekt7 zapisz obiekt złożony z obiektu 3 i obiektu 4.
+--4. Wyznacz pole powierzchni wszystkich buforów o wielkości 5 jednostek, które zostały utworzone wokół obiektów nie zawierających łuków.
+
+--1
+SELECT ST_Area(ST_Buffer(ST_ShortestLine(geometry1.geometria, geometry2.geometria), 5)) 
+FROM obiekty geometry1, obiekty geometry2 WHERE geometry1.nazwa='obiekt3' AND geometry2.nazwa='obiekt4';
+
+--2
+UPDATE obiekty SET geometria = ST_MAKEPOLYGON(ST_AddPoint(geometria, ST_StartPoint(geometria))) WHERE nazwa = 'obiekt4';
+
+--3
+INSERT INTO obiekty 
+VALUES ('obiekt7', ST_GeomFromText('GEOMETRYCOLLECTION(LINESTRING(10 17, 7 15),LINESTRING(7 15, 12 13),LINESTRING(12 13, 10 17),
+								   LINESTRING(20 20, 25 25),LINESTRING(25 25, 27 24),LINESTRING(27 24, 25 22), LINESTRING(25 22, 26 21),
+								   LINESTRING(26 21, 22 19),LINESTRING(22 19, 20.5 19.5))', -1));
+
+--4
+SELECT nazwa, ST_Area(ST_Buffer(geometria, 5)) FROM obiekty WHERE NOT ST_HasArc(geometria);
